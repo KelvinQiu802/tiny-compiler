e57a051efd40c9b765f70679f111b29f3707d455
KelvinQiu <kelvinqiu802@outlook.com>
2022-10-17 22:41:55 +0800

    feat: test

--- tiny-compiler.js
+++ tiny-compiler.js
@@ -323,3 +323,8 @@ function compiler(input) {
   const newAst = transformer(ast);
   return codeGenerator(newAst);
 }
+
+// TEST
+console.log(compiler('(add 10 (subtract 20 100)) (connect "Hello" "World")'));
+// add(10, subtract(20, 100));
+// connect("Hello", "World");
